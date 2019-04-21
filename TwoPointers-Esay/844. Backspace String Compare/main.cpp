c9b18df1a2e1b9d18dd52957217f49a41e7f7a3e
Esrappelt <1090822794@qq.com>
2019-04-21 17:57:39 +0800

    2019/04/21 17:57:38

--- TwoPointers-Esay/844. Backspace String Compare/main.cpp
+++ TwoPointers-Esay/844. Backspace String Compare/main.cpp
@@ -6,10 +6,26 @@ using namespace std;
 class Solution {
 public:
     bool backspaceCompare(string S, string T) {
-        
+        stack<int> s,t;
+        for(auto key : S) {
+            if(key != '#') s.push(key);
+            else if(!s.empty()) s.pop();
+        }
+        for(auto key : T) {
+            if(key != '#') t.push(key);
+            else if(!t.empty()) t.pop();
+        }
+        if(s.size() != t.size()) return false;
+        while(!s.empty() && !t.empty()) {
+            if(s.top() != t.top()) return false;
+            s.pop();t.pop();
+        }
+        return true;
     }
 };
 int main() {
-
+    Solution s;
+    bool ans = s.backspaceCompare("a##c","a#d#c");
+    cout << ans << endl;
 	return 0;
 }

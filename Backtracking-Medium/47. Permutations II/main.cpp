b326bc55f5583e9b144d62a1b206945ab968f942
Esrappelt <1090822794@qq.com>
2019-05-11 23:12:58 +0800

    2019/05/11 23:12:57

--- Backtracking-Medium/47. Permutations II/main.cpp
+++ Backtracking-Medium/47. Permutations II/main.cpp
@@ -5,19 +5,42 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-		set<vector<int>> ans;
-		vector<vector<int>> res;
-		sort(nums.begin(),nums.end());
-        do{ans.insert(nums);}while(next_permutation(nums.begin(),nums.end()));
-		for(auto it : ans) res.emplace_back(it); 
-		return res;
+			set<vector<int>> ans;
+			vector<vector<int>> res;
+			sort(nums.begin(),nums.end());
+					do{ans.insert(nums);}while(next_permutation(nums.begin(),nums.end()));
+			for(auto it : ans) res.emplace_back(it); 
+			return res;
     }
 };
 
 
+class Solution2 {
+public:
+		vector<vector<int>> res;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+			sort(nums.begin(),nums.end());
+			permute(nums,0);
+			return res;
+    }
+		void permute(vector<int> nums,int i) {
+			if(i >= nums.size()) {
+				res.emplace_back(nums);
+				return;
+			}
+			for(int j = i; j < nums.size(); j++) {
+				if(j == i || nums[i] != nums[j]) {
+					swap(nums[i],nums[j]);
+					permute(nums,i+1);
+				}
+			}
+		}
+};
+
+
 int main() {
-	Solution s;
-	vector<int> nums = {1,1,2}; 
+	Solution2 s;
+	vector<int> nums = {1,1,2,2}; 
 	vector<vector<int>> res = s.permuteUnique(nums);
 	for(auto it : res) {
 		for(auto jt : it) cout << jt << ' ';
